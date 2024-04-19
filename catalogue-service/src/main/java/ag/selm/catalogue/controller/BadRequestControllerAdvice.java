218f23ddb4d36b3e27ba9325e7f397d5637850ec
Stanislav <stason1986@mail.ru>
2024-04-19 11:54:15 +0300

    add jpa repo and postgres in docker image

--- catalogue-service/src/main/java/ag/selm/catalogue/controller/BadRequestControllerAdvice.java
+++ catalogue-service/src/main/java/ag/selm/catalogue/controller/BadRequestControllerAdvice.java
@@ -28,6 +28,5 @@ public class BadRequestControllerAdvice {
                         .toList());
         return ResponseEntity.badRequest()
                 .body(problemDetail);
-
     }
 }
--- catalogue-service/src/main/java/ag/selm/catalogue/service/DefaultProductService.java
+++ catalogue-service/src/main/java/ag/selm/catalogue/service/DefaultProductService.java
@@ -4,23 +4,27 @@ import ag.selm.catalogue.entity.Product;
 import ag.selm.catalogue.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class DefaultProductService implements ProductService{
+public class DefaultProductService implements ProductService {
 
     private final ProductRepository productRepository;
 
     @Override
-    public List<Product> findAllProducts() {
+    public Iterable<Product> findAllProducts(String filter) {
+        if (filter != null && !filter.isBlank()) {
+            return productRepository.findAllByTitleLikeIgnoreCase("%" + filter + "%");
+        }
         return productRepository.findAll();
     }
 
     @Override
+    @Transactional
     public Product createProduct(String title, String details) {
         return this.productRepository.save(new Product(null, title, details));
     }
@@ -31,6 +35,7 @@ public class DefaultProductService implements ProductService{
     }
 
     @Override
+    @Transactional
     public void updateProduct(Integer id, String title, String details) {
         this.productRepository.findById(id)
                 .ifPresentOrElse(product -> {
@@ -42,6 +47,7 @@ public class DefaultProductService implements ProductService{
     }
 
     @Override
+    @Transactional
     public void deleteProduct(Integer id) {
         this.productRepository.deleteById(id);
     }
